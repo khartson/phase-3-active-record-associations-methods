28d6f555cec9dde61c070f141c942c6a266dc859
Kyle <khartson2017@gmail.com>
2022-06-11 16:49:07 -0400

    completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,17 +5,22 @@ Artist.delete_all
 hotline_bling = Song.create(name: 'Hotline Bling')
 thriller = Song.create(name: 'Thriller')
 
+kelly_price = Song.create(name: "Kelly Price")
+
 drake = Artist.create(name: 'Drake')
 mj = Artist.create(name: 'Michael Jackson')
+migos = Artist.create(name: 'Migos')
 
 rap = Genre.create(name: 'Rap')
 pop = Genre.create(name: 'Pop')
 
 hotline_bling.artist = drake
 thriller.artist = mj
+kelly_price.artist = migos
 
 drake.songs << hotline_bling
 mj.songs << thriller
 
 pop.songs << thriller
 rap.songs << hotline_bling
+rap.songs << kelly_price
